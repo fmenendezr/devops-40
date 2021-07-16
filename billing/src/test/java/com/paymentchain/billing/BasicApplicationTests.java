b4095880f73d997bd8d1aa75532460776a630f1d
fmenendezr <2609fmr@gmail.com>
2021-07-16 16:47:47 -0500

    add test modification 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
  @Test
  public void contextLoads() {
-	         String message= "default message cambio devops40-2";
+	         String message= "default message cambio devops40-12";
 			 Assert.assertNotNull(message);
 	}
 
